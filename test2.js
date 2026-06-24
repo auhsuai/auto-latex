c627dc0648bc467ba7e7c488d4aef11b1f251f44
auhsuai <auhsuai@users.noreply.github.com>
2026-06-24 15:08:36 +0700

    Remove test files

--- test2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/core/converter.ts', 'utf-8');
-// extract functions
-eval(content.replace(/export /g, '').replace(/import .*?from .*?;/g, ''));
-
-const input = "F(\\omega) = e^{-2i\\omega} G(\\omega) = \\frac{e^{-2i\\omega}}{3+i\\omega}";
-const output = sanitizeLaTeX(input, true);
-console.log("INPUT: " + input);
-console.log("OUTPUT: " + output);
